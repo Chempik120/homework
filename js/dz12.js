0f3a60697ef345e2c994e35b96f0bd38d6e65bfb
Chempik120 <135869484+Chempik120@users.noreply.github.com>
2024-10-11 19:18:49 +0300

    ddd

---
+++ js/dz12.js
@@ -0,0 +1,55 @@
+const plusMoney = document.querySelector(".plusMoney");
+const gotMoney = document.querySelector(".gotMoney");
+const plusButton = document.querySelector(".plusButton");
+const gotButton = document.querySelector(".gotButton");
+const weatherCheck = document.querySelector(".weatherCheck");
+const ratingCheck = document.querySelector(".ratingCheck");
+
+
+
+const bankAccount = {
+    ownerName : "Andrey",
+    accountNumber : "12312312314",
+    balance : 370,
+    deposit: function(money){
+     bankAccount.balance = Number.parseFloat(bankAccount.balance) + Number.parseFloat(plusMoney.value) + "$";
+     console.log('Your balance', + this.balance);
+    }
+};
+
+plusButton.addEventListener("click",bankAccount.deposit);
+
+
+
+const weather = {
+    temperature: 25,
+    humidity:"",
+    windSpeed:"20 km/ph",
+    tempMessage: function(){
+        const temp = prompt("Введіть температуру");
+        if(temp < 0){
+           alert("Температура нижча 0 градусів Цельсія")
+        } else if(temp >= 0) {
+         alert("Температура вища за 0 градусів Цельсія")
+        }
+    }
+}
+
+weatherCheck.addEventListener("click", weather.tempMessage);
+
+const movie = {
+    title:"Movie",
+    director:"Andrew",
+    year:"2017",
+    rating: 9 ,
+    checkRating: function(){
+        if(this.rating >= 8){
+            console.log("true")
+        } else if(this.rating < 8){
+            console.log("false")
+        };
+        console.log(movie.title,movie.director,movie.year,movie.rating);
+    }
+};
+
+ratingCheck.addEventListener("click", movie.checkRating);
