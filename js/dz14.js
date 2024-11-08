c99c805c8d1f744482661f29fb0b0904aee3b0d5
Chempik120 <135869484+Chempik120@users.noreply.github.com>
2024-11-08 19:23:33 +0200

    і

--- js/dz14.js
+++ js/dz14.js
@@ -94,4 +94,53 @@
 
 
 
-console.log(getInactiveUsers(users))  
+console.log(getInactiveUsers(users));
+
+const getUserNames = users => users.map(user => user.name);
+
+console.log(getUserNames(users));
+
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+
+console.log(getUsersWithEyeColor(users, 'blue'));
+
+
+const getUsersWithGender = (users, gender) => users.filter(user => user.gender === gender).map(user => user.name);
+
+console.log(getUsersWithGender(users, 'male'));
+
+
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
+
+console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
+
+
+const getUsersWithAge = (users, min, max) => users.filter(user => user.age >= min && user.age <= max);
+
+console.log(getUsersWithAge(users, 20, 30));
+
+console.log(getUsersWithAge(users, 30, 40));
+
+
+const calculateTotalBalance = users => users.reduce((total, user) => total + user.balance, 0);
+
+console.log(calculateTotalBalance(users));
+
+
+
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName)).map(user => user.name);
+
+console.log(getUsersWithFriend(users, 'Briana Decker'));
+
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+
+const getNamesSortedByFriendsCount = users => users.slice().sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
+
+console.log(getNamesSortedByFriendsCount(users));
+
+
+const getSortedUniqueSkills = users => [...new Set(users.flatMap(user => user.skills))].sort();
+
+console.log(getSortedUniqueSkills(users));
